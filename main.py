49d9c09b72829d29ebf5cefc3b805de29f7db7a5
dionna <dionnajacobson@gmail.com>
2022-09-29 20:57:21 -0400

    commit fastapi code

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('hello AWS world!')
+from fastapi import FastAPI
+
+app = FastAPI()
+
+#The root path will be used as the health check endpoint
+@app.get("/", tags=["Health Check"])
+async def root():
+    return {"message": "Service is online."}
